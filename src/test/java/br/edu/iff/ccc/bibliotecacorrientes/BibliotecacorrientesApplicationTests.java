956f39df961c237d05dcb69e0af9f6cb485ac985
Caio Pontes <pontes.caio@gsuite.iff.edu.br>
2026-06-28 14:18:33 -0300

    Preparação Estrutura Projeto

---
+++ src/test/java/br/edu/iff/ccc/bibliotecacorrientes/BibliotecacorrientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.bibliotecacorrientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecacorrientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
